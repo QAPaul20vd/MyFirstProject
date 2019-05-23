54700fa9f6bbefe7b39e321b372e542de8aefe96
QAPaul20vd <paulbvd@gmail.com>
2019-05-23 20:39:12 +0300

    Codul meu

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code herS
+        System.out.println("Hello, World");
+        byte myByte = 34;
+        byte Nrsd = 2;
+        int result = myByte + Nrsd;
+        System.out.println(result);
+
+        String answer = "The result is: ";
+
+        System.out.println(answer + result);
+
+        float virgula = 43.23f;
+        double rezultat = virgula + Nrsd;
+        System.out.println(rezultat);
+
+    }
+}
